fc03e26cd5a6cacb09136226cd81d7c3a16a78a1
Compuglobal <admin@compuglobal.net>
2025-03-29 10:47:24 -0700

    revisions to automation action.

--- Formula/avbroot.rb
+++ Formula/avbroot.rb
@@ -1,16 +1,12 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-
-class Avbroot < Formula
+class avbroot < Formula
   desc "Sign (and root) Android A/B OTAs with custom keys while preserving Android Verified Boot"
+  version "3.14.0"
   homepage "https://github.com/chenxiaolong/avbroot"
   url "https://github.com/chenxiaolong/avbroot/releases/download/v3.14.0/avbroot-3.14.0-universal-apple-darwin.zip"
-  sha256 "6fa6a435aa7592c39bd27a3a2bdc958127c25713166a7708ae1bcfde4ca57f08"
-  version "3.14.0"
+  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
   license "GPL-3.0"
 
   def install
     bin.install "avbroot"
   end
-
 end
