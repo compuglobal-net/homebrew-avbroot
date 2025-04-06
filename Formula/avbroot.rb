e85f0d9644cb3610f704f9a09b0fb32631eb00e8
Compuglobal <admin@compuglobal.net>
2025-04-06 10:15:07 +0000

    New avbroot release v3.15.0

--- Formula/avbroot.rb
+++ Formula/avbroot.rb
@@ -1,9 +1,9 @@
 class Avbroot < Formula
   desc "Sign (and root) Android A/B OTAs with custom keys while preserving Android Verified Boot"
-  version "3.14.0"
+  version "3.15.0"
   homepage "https://github.com/chenxiaolong/avbroot"
-  url "https://github.com/chenxiaolong/avbroot/releases/download/v3.14.0/avbroot-3.14.0-universal-apple-darwin.zip"
-  sha256 "6fa6a435aa7592c39bd27a3a2bdc958127c25713166a7708ae1bcfde4ca57f08"
+  url "https://github.com/chenxiaolong/avbroot/releases/download/v3.15.0/avbroot-3.15.0-universal-apple-darwin.zip"
+  sha256 "d30b92c291f0610feb148c63b53df4ab8f84da4dba85c2ed98ec6474687f8038"
   license "GPL-3.0"
 
   def install
