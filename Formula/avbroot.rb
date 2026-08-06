be37814ce472136ce00175d0d148d9eff20bebdf
Compuglobal <admin@compuglobal.net>
2026-08-06 11:59:24 +0000

    New avbroot release v3.33.0

--- Formula/avbroot.rb
+++ Formula/avbroot.rb
@@ -1,9 +1,9 @@
 class Avbroot < Formula
   desc "Tool for manipulating and re-signing Android A/B OTAs"
-  version "3.32.2"
+  version "3.33.0"
   homepage "https://github.com/chenxiaolong/avbroot"
-  url "https://github.com/chenxiaolong/avbroot/releases/download/v3.32.2/avbroot-3.32.2-universal-apple-darwin.zip"
-  sha256 "202d54f2b2111bdb544ee510b3696e9e62a4f7f775fa42c255683248a6bcf326"
+  url "https://github.com/chenxiaolong/avbroot/releases/download/v3.33.0/avbroot-3.33.0-universal-apple-darwin.zip"
+  sha256 "56d40b27ab2409da773e03e9c9f386a75cccd879183c232f9ab18b00f0288fec"
   license "GPL-3.0"
 
   def install
